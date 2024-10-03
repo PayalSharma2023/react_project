b327da1c79e588a805b2a2dec9b8122a88796be6
Payal Sharma <payalsharmareal@gmail.com>
2024-10-03 20:29:44 +0530

    Basic implementation of chakra ui

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -3,16 +3,13 @@ import Nav from './components/Nav';
 import * as React from 'react'
 
 // 1. import `ChakraProvider` component
-import { ChakraProvider } from '@chakra-ui/react'
 import Faq from './components/FAQ';
 
 function App() {
 
   return (
     <>
-      <ChakraProvider>
-      <Faq />
-     </ChakraProvider>
+      <Faq></Faq>
     </>
   )
 }
