21666d244abd8d58c70c59d3273d5e8f24f01619
Payal Sharma <payalsharmareal@gmail.com>
2024-10-05 09:39:22 +0530

    Grid and List

---
+++ Chakra-UI-Crash_course/src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import {CalendarIcon} from "@chakra-ui/icons";
+import { List, ListItem, ListIcon } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <List color="white" fontSize="1.2em" spacing={4}>
+      <ListItem>
+        <NavLink to="/">
+        <ListIcon as={CalendarIcon} color="white"/>
+        Dashboard</NavLink>
+      </ListItem>
+      <ListItem>
+        <NavLink to="/create">New Task</NavLink>
+      </ListItem>
+      <ListItem>
+        <NavLink to="/profile">Profile</NavLink>
+      </ListItem>
+    </List>
+  );
+}
